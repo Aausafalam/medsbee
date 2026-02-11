a31400b7a86f062b2264a9b7ed12ad6fdffc8145
aausaf <aausaf@c3ihub.iitk.ac.in>
2026-02-11 20:50:22 +0530

    [update]

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,19 @@ const nextConfig = {
     experimental: {
         optimizeCss: true,
     },
+    async headers() {
+        return [
+            {
+                source: "/:path*(svg|png|jpg|jpeg|webp|woff2|ico)",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=31536000, immutable",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
