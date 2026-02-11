a902c8b39edb90f24f05a066b42cec991b786726
aausaf <aausaf@c3ihub.iitk.ac.in>
2026-02-11 20:10:16 +0530

    [update]

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,13 @@ import "./globals.css";
 const geistSans = Geist({
     subsets: ["latin"],
     variable: "--font-sans",
+    display: "swap",
 });
 
 const geistMono = Geist_Mono({
     subsets: ["latin"],
     variable: "--font-mono",
+    display: "swap",
 });
 
 export const metadata: Metadata = {
