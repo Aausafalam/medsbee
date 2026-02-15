08639b258a7c6c42f32358290e9afe5ff1d835a0
aausaf <aausaf@c3ihub.iitk.ac.in>
2026-02-15 19:56:54 +0530

    [update]

--- data/footer.ts
+++ data/footer.ts
@@ -1,23 +1,22 @@
 export const footerData = {
-  logo: 'MedsBee',
-  description:
-    'Super practical courses, with a no-nonsense approach, that are designed to help you take financial decisions from a point of awareness, and not ignorance.',
-  social: [
-    { name: 'LinkedIn', href: 'https://in.linkedin.com/in/dr-karan-chawla-b35ba3210', icon: 'linkedin' },
-    // { name: 'YouTube', href: 'https://www.youtube.com/@MedsBee', icon: 'youtube' },
-    { name: 'Instagram', href: 'https://www.instagram.com/dr.karanchawla/?hl=en', icon: 'instagram' },
-  ],
-  support: {
-    label: 'For support',
-    icon: 'whatsapp',
-    href: 'mailto:contact@medsbee.com',
-  },
-  bottomLinks: [
-    { label: 'Privacy Policy', href: '/privacy' },
-    { label: 'Terms of Use', href: '/terms' },
-    { label: 'Contact Us', href: '/contact' },
-    // { label: 'Blogs', href: '/blogs' },
-    { label: 'Refund Policy', href: '/refund' },
-  ],
-  copyright: '© Copyright 2026',
-}
+    logo: "MedsBee",
+    description: "Super practical courses, with a no-nonsense approach, that are designed to help you take financial decisions from a point of awareness, and not ignorance.",
+    social: [
+        { name: "LinkedIn", href: "https://in.linkedin.com/in/dr-karan-chawla-b35ba3210", icon: "linkedin" },
+        // { name: 'YouTube', href: 'https://www.youtube.com/@MedsBee', icon: 'youtube' },
+        { name: "Instagram", href: "https://www.instagram.com/dr.karanchawla", icon: "instagram" },
+    ],
+    support: {
+        label: "For support",
+        icon: "whatsapp",
+        href: "mailto:contact@medsbee.com",
+    },
+    bottomLinks: [
+        { label: "Privacy Policy", href: "/privacy" },
+        { label: "Terms of Use", href: "/terms" },
+        { label: "Contact Us", href: "/contact" },
+        // { label: 'Blogs', href: '/blogs' },
+        { label: "Refund Policy", href: "/refund" },
+    ],
+    copyright: "© Copyright 2026",
+};
