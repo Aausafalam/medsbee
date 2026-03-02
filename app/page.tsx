7b686b463cd637a2d865bc5a849ca3a011679a4c
aausaf <aausaf@c3ihub.iitk.ac.in>
2026-03-02 11:01:12 +0530

    [updae]

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             <main>
                 <Hero />
                 <SocialProof />
-                <Reactions />
+                {/* <Reactions /> */}
                 <Roadmap />
                 {/* <Highlight /> */}
                 <Instructor />
